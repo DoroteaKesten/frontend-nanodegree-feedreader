f838c06ecd0cc9b81067f39479c9d0b52583370e
Dorotea Kesten <dorotea.kesten@gmail.com>
2018-08-01 22:08:00 +0200

    Describe test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,12 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has URL defined', () => {
+            allFeeds.forEach((item) => {
+                expect(item.url).toBeDefined();
+                expect(item.url).not.toBe('');
+            });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has name defined', () => {
+            allFeeds.forEach((item) => {
+                expect(item.name).toBeDefined();
+                expect(item.name).not.toBe('');
+            });
+         });
     });
 
 
